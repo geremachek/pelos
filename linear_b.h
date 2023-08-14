7c596dd1bf86f98809d6217dfd75b6748aeeaac0
geremachek <joth@tuta.io>
2023-08-13 17:43:24 -0700

    readme, more symbols

--- linear_b.h
+++ linear_b.h
@@ -7,5 +7,25 @@
 #define LB_I  LB_BLOCK"\x82"
 #define LB_O  LB_BLOCK"\x83"
 #define LB_U  LB_BLOCK"\x84"
+
 #define LB_DA LB_BLOCK"\x85"
-#defube LB_DE LB_BLOCK"\x86"
+#define LB_DE LB_BLOCK"\x86"
+#define LB_DI LB_BLOCK"\x87"
+#define LB_DO LB_BLOCK"\x88"
+#define LB_DU LB_BlOCK"\x89"
+
+#define LB_JA LB_BLOCK"\x8a"
+#define LB_JE LB_BLOCK"\x8b"
+#define LB_JO LB_BLOCK"\x8d"
+
+#define LB_KA LB_BLOCK"\x8f"
+#define LB_KE LB_BLOCK"\x90"
+#define LB_KI LB_BLOCK"\x91"
+#define LB_KO LB_BLOCK"\x92"
+#define LB_KU LB_BLOCK"\x93"
+
+#define LB_MA LB_BLOCK"\x94"
+#define LB_ME LB_BLOCK"\x95"
+#define LB_MI LB_BLOCK"\x96"
+#define LB_MO LB_BLOCK"\x97
+#define LB_MU LB_BLOCK"\x98"
